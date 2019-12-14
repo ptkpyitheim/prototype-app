c967e9793bf5137b9d6fc783eb84f8c8da5e5a10
Pyi Them Kyaw <pyithemkyaw@Pyis-MacBook-Pro.local>
2019-12-13 18:19:46 -0600

    Updates

---
+++ precache-manifest.aa102801395a2fa0aa97e68ded106344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba5db14aed8ca9b40461de5395a3eae",
+    "url": "/prototype-app/index.html"
+  },
+  {
+    "revision": "3af49ce7bec59e222632",
+    "url": "/prototype-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca38809bfd35d6ba8e34",
+    "url": "/prototype-app/static/js/2.c69a0177.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/prototype-app/static/js/2.c69a0177.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3af49ce7bec59e222632",
+    "url": "/prototype-app/static/js/main.4405237b.chunk.js"
+  },
+  {
+    "revision": "eb5e79f6cf84f3767774",
+    "url": "/prototype-app/static/js/runtime-main.784ee285.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prototype-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
